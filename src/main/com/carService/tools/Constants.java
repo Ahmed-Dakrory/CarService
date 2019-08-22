0a2236cd8a08d1b33e86d227ac4f1b689564b813
Ahmed.Dakrory <Ahmed.Dakrory@10.50.5.29>
2019-08-22 15:47:54 +0200

    change email

--- src/main/com/carService/tools/Constants.java
+++ src/main/com/carService/tools/Constants.java
@@ -2401,13 +2401,13 @@ public class Constants {
 			public void run() {
 				
 				
-				String from = "info@adainter.com";
-		        String pass = "T1234567t*";
+				String from = "adainterusa@gmail.com";
+		        String pass = "T1234567t";
 				
 				// TODO Auto-generated method stub
 				 Properties props = System.getProperties();
 
-			        String host = "smtp.office365.com";
+			        String host = "smtp.gmail.com";
 			        props.put("mail.smtp.starttls.enable", "true");
 			        props.put("mail.smtp.host", host);
 			        props.put("mail.smtp.user", from);
